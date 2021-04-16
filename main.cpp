f72047a4c4e0fd73bcb871cd9ad59e15272257ca
destrospooder <benjamin.aziel@cooper.edu>
2021-04-16 03:13:03 -0400

    dsa is the bane of my existence and i wish it nothing but pain

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <vector>
 #include <list>
+#include <map>
 using namespace std;
 
 template <typename Type> class simpleList{
@@ -11,31 +12,34 @@ template <typename Type> class simpleList{
             private:
                 Type data;
                 struct Node* next;
-            Node(Type t){
-                data = t;
-                next = nullptr;
-            }
+            public:
+                Node(Type t){
+                    data = t;
+                    next = nullptr;
+                }
 
-            Type getData() const {
-                return data;
-            }
+                Type getData() const {
+                    return data;
+                }
 
-            void setData(Type data) {
-                Node::data = data;
-            }
+                void setData(Type data) {
+                    Node::data = data;
+                }
 
-            Node *getNext() const {
-                return next;
-            }
+                Node *getNext() const {
+                    return next;
+                }
 
-            void setNext(Node *next) {
-                Node::next = next;
-            }
+                void setNext(Node *next) {
+                    Node::next = next;
+                }
         };
     protected:
         struct Node* first;
         struct Node* last;
         int size;
+        string name;
+
     public:
         simpleList(){
             first = nullptr;
@@ -51,7 +55,11 @@ template <typename Type> class simpleList{
         virtual void push(Type t) = 0;
         virtual Type pop() = 0;
 
-        // cap = attaching the node at the front
+        const string &getName() const {
+            return name;
+        }
+
+    // cap = attaching the node at the front
         void cap(Type t){
             Node* index = new Node(t);
             if(isEmpty()){
@@ -83,9 +91,9 @@ template <typename Type> class simpleList{
         Type pour(){
             if(isEmpty()){
                 cout << "it brokey :(\n";
-                return 420;
+                return 0;
             }
-            Type hopper = first -> getValue();
+            Type hopper = first -> getData();
             if(size == 1) {
                 size = size - 1;
                 first = last = nullptr;
@@ -101,9 +109,11 @@ template <typename Type> class simpleList{
 
 // Implemented Stack and Queue using Cap, Cup, and Pour!
 
+
 template <typename Type> class stack:public simpleList<Type>{
     public:
-        void push(Type t){
+
+    void push(Type t){
             this -> cap(t);
         }
         Type pop(){
@@ -113,7 +123,8 @@ template <typename Type> class stack:public simpleList<Type>{
 
 template <typename Type> class queue:public simpleList<Type>{
     public:
-        void push(Type t){
+
+    void push(Type t){
             this -> cup(t);
         }
         Type pop(){
@@ -121,44 +132,136 @@ template <typename Type> class queue:public simpleList<Type>{
         }
 };
 
-vector<string> tokenizer(string s){
-    vector<string> divide;
-    stringstream stream(s);
-    string reader;
-    while(stream >> reader) {
-        divide.push_back(reader);
-        cout << reader << "\n";
+void tokenizer(string box[], ifstream& input){
+    input >> box[0];
+    if(box[0] == "pop"){
+        input >> box[1];
     }
-    return divide;
+    else{
+        input >> box[1];
+        input >> box[2];
+    }
 }
 
 //Basic file reader
 //Opens input, creates output, prints and stores strings
+
+
+//template<typename Type> simpleList <Type> listFind(string label, list<simpleList<Type>*> labels){
+//    for(typename list<simpleList<Type>*> :: iterator it = labels.begin(); it != labels.end(); it++){
+//        if(it.getName() == label) {
+//            return it;
+//        }
+//    }
+//}
+
+bool duplicateFind(string label, list<string> labels){
+    for(string &listlabel : labels){
+        if (listlabel == label){
+            return true;
+        }
+    }
+    return false;
+}
+
 void billy() {
-    list < simpleList<int> *> intList;
-    list < simpleList<double> *> doubleList;
-    list < simpleList<string> *> stringList;
+    map <string, simpleList<int> *> intList;
+    map <string, simpleList<double> *> doubleList;
+    map <string, simpleList<string> *> stringList;
     vector<string> tokens;
-    string primero;
     ifstream input;
     ofstream output;
+    list<string> labelList;
     input.open("input.txt");
     output.open("output.txt");
-    string stored;
-    while (getline(input, stored)) {
-        tokens = tokenizer(stored);
-        primero = tokens[0];
-        cout << stored << "\n";
-        output << stored << "\n";
-        if(primero == "create"){
-
+    string box[3];
+    string bob;
+    while (input.peek() != EOF) {
+        output << "PROCESSING COMMAND: " << input.peek() << "\n";
+        tokenizer(box, input);
+        cout << box[0] << box[1] << box[2] << "\n";
+        if (box[0] == "create"){
+            if (duplicateFind(box[1], labelList)){
+                output << "ERROR: This name already exists!\n";
+                continue;
+            }
+            labelList.push_front(box[1]);
+            if (box[2] == "stack"){
+                if (box[1].substr(0,1) == "i") {
+                    intList.insert(std::pair<string,simpleList<int>*>(box[1],new stack<int>()));
+                }
+                if (box[1].substr(0,1) == "d") {
+                    doubleList.insert(std::pair<string,simpleList<double>*>(box[1],new stack<double>()));
+                }
+                if (box[1].substr(0,1) == "s") {
+                    stringList.insert(std::pair<string,simpleList<string>*>(box[1],new stack<string>()));
+                }
+            }
+            if (box[2] == "queue"){
+                if (box[1].substr(0,1) == "i") {
+                    intList.insert(std::pair<string,simpleList<int>*>(box[1],new queue<int>()));
+                }
+                if (box[1].substr(0,1) == "d") {
+                    doubleList.insert(std::pair<string,simpleList<double>*>(box[1],new queue<double>()));
+                }
+                if (box[1].substr(0,1) == "s") {
+                    stringList.insert(std::pair<string,simpleList<string>*>(box[1],new queue<string>()));
+                }
+            }
+        }
+        if (box[0] == "push"){
+            if (!duplicateFind(box[1], labelList)){
+                continue;
+            }
+            if (box[1].substr(0,1) == "i"){
+                intList[box[1]] -> push(stoi(box[1]));
+                continue;
+            }
+            if (box[1].substr(0,1) == "d"){
+                doubleList[box[1]] -> push(stod(box[1]));
+                continue;
+            }
+            if (box[1].substr(0,1) == "s"){
+                stringList[box[1]] -> push(box[1]);
+                continue;
+            }
+        }
+        if (box[0] == "pop"){
+            if (!duplicateFind(box[1], labelList)) {
+                continue;
+            }
+            if (box[1].substr(0,1) == "i"){
+                if(intList[box[1]] -> isEmpty()){
+                    continue;
+                }
+                intList[box[1]] -> pop();
+                continue;
+            }
+            if (box[1].substr(0,1) == "d"){
+                if(doubleList[box[1]] -> isEmpty()){
+                    continue;
+                }
+                doubleList[box[1]] -> pop();
+                continue;
+            }
+            if (box[1].substr(0,1) == "s"){
+                if(stringList[box[1]] -> isEmpty()){
+                    continue;
+                }
+                stringList[box[1]] -> pop();
+                continue;
+            }
         }
     }
     input.close();
     output.close();
 }
 
+//Read in one word at a time. If doing create or push, read in two more words; if doing pull, read in one.
+//Create some function that does the create/pop/push stuff using two arguments each
+
 int main(){
+    billy();
     return 0;
 }
 
